046e5b56c618184fe652da220f78127235a01560
vichet vatt <vichetvattedu@gmail.com>
2022-09-29 22:48:24 +0700

    Worked with Function

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,16 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  //
+  //
   void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
+  }
+
+//Use {} if we don't want to confuse to pass the value
+  void showName({String greeting, String name}) {
+    print('$greeting $name');
   }
 
   @override
@@ -17,59 +24,94 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Container(
             alignment: Alignment.center,
             child: Column(
-              children: [
-                FlatButton(
-                  color: Colors.red,
-                  child: Text("Click Here 1"),
-                  onPressed: () {
-                    playSound(1);
-                  },
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.red,
+                    child: Text("Click Here 1"),
+                    onPressed: () {
+                      playSound(1);
+                      showName(greeting: 'hello', name: 'vichet');
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.orange,
-                  child: Text("Click Here 2"),
-                  onPressed: () {
-                    playSound(2);
-                  },
+                SizedBox(
+                  height: 15,
                 ),
-                FlatButton(
-                  color: Colors.yellow,
-                  child: Text("Click Here 3"),
-                  onPressed: () {
-                    playSound(3);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.orange,
+                    child: Text("Click Here 2"),
+                    onPressed: () {
+                      playSound(2);
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.green,
-                  child: Text("Click Here 4"),
-                  onPressed: () {
-                    playSound(4);
-                  },
+                SizedBox(
+                  height: 15,
                 ),
-                FlatButton(
-                  color: Colors.greenAccent,
-                  child: Text("Click Here 5"),
-                  onPressed: () {
-                    playSound(5);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.yellow,
+                    child: Text("Click Here 3"),
+                    onPressed: () {
+                      playSound(3);
+                    },
+                  ),
                 ),
-                FlatButton(
-                  color: Colors.blue,
-                  child: Text("Click Here 6"),
-                  onPressed: () {
-                    playSound(6);
-                  },
+                SizedBox(
+                  height: 15,
                 ),
-                FlatButton(
-                  color: Colors.purple,
-                  child: Text("Click Here 7"),
-                  onPressed: () {
-                    playSound(7);
-                  },
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.green,
+                    child: Text("Click Here 4"),
+                    onPressed: () {
+                      playSound(4);
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.greenAccent,
+                    child: Text("Click Here 5"),
+                    onPressed: () {
+                      playSound(5);
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.blue,
+                    child: Text("Click Here 6"),
+                    onPressed: () {
+                      playSound(6);
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.purple,
+                    child: Text("Click Here 7"),
+                    onPressed: () {
+                      playSound(7);
+                    },
+                  ),
                 ),
               ],
             ),
