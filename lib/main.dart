4d76a1558bfc39d0c434006f665acbdb959f84e0
vichet vatt <vichetvattedu@gmail.com>
2022-10-01 10:20:49 +0700

    Finished Xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -21,32 +21,32 @@ class XylophoneApp extends StatelessWidget {
 //     print('$greeting $name');
 //   }
 
-  void buildKey() {
-    Expanded(
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
       child: FlatButton(
-        color: Colors.red,
+        color: color,
         child: Text("Click Here 1"),
         onPressed: () {
-          playSound(1);
+          playSound(soundNumber);
         },
       ),
     );
   }
 
-  void main() {
-    int step1Result = add(n1: 5, n2: 9);
-    int step2Result = multiply(step1Result, 5);
-    double finalResult = step2Result / 3;
-    print(finalResult);
-  }
+  // void main() {
+  //   int step1Result = add(n1: 5, n2: 9);
+  //   int step2Result = multiply(step1Result, 5);
+  //   double finalResult = step2Result / 3;
+  //   print(finalResult);
+  // }
 
-  int add({int n1, int n2}) {
-    return (n1 + n2);
-  }
+  // int add({int n1, int n2}) {
+  //   return (n1 + n2);
+  // }
 
-  int multiply(int n1, int n2) {
-    return (n1 * n2);
-  }
+  // int multiply(int n1, int n2) {
+  //   return (n1 * n2);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -60,16 +60,19 @@ class XylophoneApp extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Expanded(
-                  child: FlatButton(
-                    color: Colors.red,
-                    child: Text("Click Here 1"),
-                    onPressed: () {
-                      playSound(1);
-                      main();
-                    },
-                  ),
-                ),
+                buildKey(color: Colors.red, soundNumber: 1),
+                SizedBox(height: 15),
+                buildKey(color: Colors.orange, soundNumber: 2),
+                SizedBox(height: 15),
+                buildKey(color: Colors.yellow, soundNumber: 3),
+                SizedBox(height: 15),
+                buildKey(color: Colors.green, soundNumber: 4),
+                SizedBox(height: 15),
+                buildKey(color: Colors.greenAccent, soundNumber: 5),
+                SizedBox(height: 15),
+                buildKey(color: Colors.blue, soundNumber: 6),
+                SizedBox(height: 15),
+                buildKey(color: Colors.purple, soundNumber: 7),
               ],
             ),
           ),
